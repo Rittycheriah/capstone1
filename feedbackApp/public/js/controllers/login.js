d38efb62a1602d472501ed66169b7ed4bb1bc540
rittycheriah <rittycheriah@gmail.com>
2015-07-03 11:33:23 -0500

    changed layout for structure of all other pages

--- feedbackApp/public/js/controllers/login.js
+++ feedbackApp/public/js/controllers/login.js
@@ -1,6 +1,5 @@
 angular.module('myApp.loginController', [])
 	.controller('loginCtrl', function ($scope, $http, $location, $rootScope) {
-		$scope.message = 'Login Below!'
 	  $scope.user = {};
 
 	  $scope.login = function() {
